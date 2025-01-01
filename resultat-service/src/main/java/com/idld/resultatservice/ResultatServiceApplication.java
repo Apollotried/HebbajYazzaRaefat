11d29683e6ae33ef37bf0afed61cc34600cdf154
Apollotried <safeapollogemail@gmail.com>
2025-01-01 23:07:48 +0100

    Template

---
+++ resultat-service/src/main/java/com/idld/resultatservice/ResultatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idld.resultatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResultatServiceApplication.class, args);
+    }
+
+}
